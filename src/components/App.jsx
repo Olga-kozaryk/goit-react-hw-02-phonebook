e6993f865d968d0027600ab5e270e394401a515a
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2023-12-12 11:46:40 +0200

    data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+import data from "./data.json"
+class App extends Component{
+    state = {
+        contacts: data,
+        name: ''
+      }
+}
+export default App
