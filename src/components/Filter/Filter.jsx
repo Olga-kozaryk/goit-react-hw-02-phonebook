ef6548a6fd3056ddb8d62dd907acd6a62c3e8546
Olha Kozaryk <114346084+Olga-kozaryk@users.noreply.github.com>
2023-12-14 08:49:31 +0200

    app filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,14 +1,15 @@
+import { FilterLabel } from "./FilterLabel.styled";
+
 export const Filter = ({value, onChange}) => {
-    return (<div >
-      <label >
+    return ( <div >
+      <FilterLabel>
         Filter
         <input
           type="name"
           value={value}
-          onChange={onChange}
-         
+          onChange={onChange}      
         />
-      </label>
+      </FilterLabel>
     </div>
   );
 }
